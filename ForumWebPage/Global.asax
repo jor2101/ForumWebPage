fcd4d3df50b5422663efd1a89996270c668a07b7
Jesus <Jesus@DESKTOP-MU2KDA5>
2017-11-17 14:58:37 -0400

    Add project files.

---
+++ ForumWebPage/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ForumWebPage.MvcApplication" Language="C#" %>
---
+++ ForumWebPage/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ForumWebPage.Startup))]
+namespace ForumWebPage
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
