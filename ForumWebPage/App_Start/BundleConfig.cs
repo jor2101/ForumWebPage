fe6f4e40d07b9a64ec714081420c96b4a4ee3ff5
Frank <Frank@LAPTOP-VV997RMT>
2017-11-17 20:01:38 -0400

    Initialized Database and Migrations

--- ForumWebPage/App_Start/BundleConfig.cs
+++ ForumWebPage/App_Start/BundleConfig.cs
@@ -24,8 +24,7 @@ namespace ForumWebPage
                       "~/Scripts/respond.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      
-                      "~/Content/bootstrapSandStone.css",
+                      "~/Content/bootstrap.css",
                       "~/Content/site.css"));
         }
     }
